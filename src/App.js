0b467d65c85b7d370095e7439b856586272267a5
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2023-11-08 20:17:01 +1000

    added fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import { Component } from 'react'
 import './App.css';
+class App extends Component {
+    constructor() {
+        super();
+         
+        this.state = {
+            monsters: []
+        }
+        this.URL = 'https://jsonplaceholder.typicode.com/users'
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    componentDidMount() {
+        fetch(this.URL)
+            .then(response => response.json())
+            .then((users) => this.setState(()=> {
+                return{ monsters: users}
+            }))
+            
+    }
+
+    render() {
+        return(
+            <div className='App'>
+                {this.state.monsters.map((monster) => {
+                    return(
+                        <div key={monster.id}>
+                            <h1>{monster.name}</h1>
+                        </div>
+                    );
+                })}
+            </div>
+        );
+    }
 }
 
 export default App;
